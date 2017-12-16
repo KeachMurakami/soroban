5c09434f01cb0974a008c8e8c430885b0b11eb29
MurakamiKeach <m.keach.11@gmail.com>
2017-12-16 17:37:48 +0900

    photoperiod: fix a bug and deals with repeated day-night

--- R/daynight.R
+++ R/daynight.R
@@ -7,14 +7,19 @@ photoperiod <-
       stringr::str_pad(., width = 2, side = "left", pad = "0") %>%
       paste0("_", ., ":00:00")
 
-    turning_points <-
+    dates_contained <-
       .tbl %>%
       pull(rlang::UQ(time_col)) %>%
       range %>%
       as.Date %>%
-      tidyr::full_seq(period = 1) %>%
-      rep(., 2) %>%
-      paste0(., turning_times) %>%
+      tidyr::full_seq(period = 1)
+
+
+    turning_points <-
+      paste0(
+        rep(dates_contained, each = length(turning_times)),
+        rep(turning_times, times = length(dates_contained))
+      ) %>%
       matrix(., ncol = 2, byrow = T)
 
     turning_points[,1] %>%
@@ -24,3 +29,13 @@ photoperiod <-
         annotate(geom = "rect", xmin = temp[1], xmax = temp[2], ...)
       })
   }
+
+# test_data <-
+#   read_csv("~/Dropbox/2017/20171213/pam/summary.csv")
+#
+# test_data %>%
+#   ggplot(aes(time, y2, col = location_id)) +
+#   photoperiod(test_data, time, day_init_end = c(7, 23), ymin = -Inf, ymax = Inf, fill = "white") +
+#   geom_line() +
+#   geom_errorbar(aes(ymin = y2 - y2_sd, ymax = y2 + y2_sd)) +
+#   geom_point()
