e3bd05f53370aee0680cd7be560a5b0df75a7ef8
MurakamiKeach <m.keach.11@gmail.com>
2017-12-22 21:30:12 +0900

    major change in `trace()`: now calculates an initial value (e.g. time = 0 value) by - linear regression from head(n) data (default; method = "reg") - calculating means from head(n) data (method = "mean")

--- R/core.R
+++ R/core.R
@@ -1,7 +1,10 @@
 trace <-
-  function(.tbl, column, n, skip = 0, fun = "mean"){
+  function(.tbl, y_reg, x_reg, n, skip, method = "reg", ...){
     target <-
-      rlang::enquo(column)
+      rlang::enquo(y_reg)
+    regressor <-
+      rlang::enquo(x_reg)
+
     target_init <-
       paste0("init_", quo_name(target))
     target_delta <-
@@ -9,18 +12,79 @@ trace <-
     target_ratio <-
       paste0("ratio_", quo_name(target))
 
-    fun <- rlang::sym(fun)
-
     rows_extracted <-
       tail(1:(n+skip), n)
 
-    values <-
+    y_values <-
       .tbl %>%
       dplyr::slice(rows_extracted) %>%
       dplyr::pull(!! target)
 
+    x_values <-
+      .tbl %>%
+      dplyr::slice(rows_extracted) %>%
+      dplyr::pull(!! regressor)
+
+
+    initial_value <-
+      data_frame(
+        x = x_values,
+        y = y_values) %>%
+      lm(data = ., y ~ x, ...) %>%
+      tidy %$%
+      estimate %>%
+      .[1]
+
+    if(method == "mean"){
+      initial_value <-
+        y_values %>%
+        mean
+    }
+
     .tbl %>%
-      mutate(!!target_init := (!!fun)(values),
+      mutate(!!target_init := initial_value,
              !!target_delta := (!!target) - (!!rlang::sym(target_init)),
              !!target_ratio := (!!target) / (!!rlang::sym(target_init)))
   }
+
+
+# function(.tbl_gr){
+#   group_merge <-
+#     .tbl_gr %>%
+#     group_vars %>%
+#     str_c(collapse = "+")
+#
+#
+#   groupsss <-
+#     rlang::quo(!!group_merge)
+#
+#   .tbl_gr %>%
+#     mutate("spliter" := rlang::eval_tidy(group_merge)) %>%
+#     select(spliter)
+#
+# }
+
+
+### demo
+
+# ddd <-
+#   data_frame(time = 0:999,
+#              var1 = 50 + seq(0, 1000, length.out = 1000) + rnorm(1000, 0, 50),
+#              var2 = 100 + seq(0, 500, length.out = 1000) + rnorm(1000, 0, 10)) %>%
+#   gather(variable, y, -time) %>%
+#   mutate(another_group = sample(letters[1:3], 2000, replace = T)) %>%
+#   group_by(variable, another_group)
+#
+# ddd %>%
+#   mutate(spliter = paste0(variable, another_group)) %>%
+#   split(.$spliter) %>%
+#   map_df(~ trace(., y, time, n = 500, skip = 0, method = "reg")) %>%
+#   ggplot(aes(time, delta_y, col = variable)) +
+#   geom_point(alpha = .25)
+#
+# ddd %>%
+#   mutate(spliter = paste0(variable, another_group)) %>%
+#   split(.$spliter) %>%
+#   map_df(~ trace(., y, time, n = 500, skip = 0, method = "mean")) %>%
+#   ggplot(aes(time, delta_y, col = variable)) +
+#   geom_point(alpha = .25)
